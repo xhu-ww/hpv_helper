4ded6db9ad35979373e78034989723c452404e90
wenwang <xhuww2012@gmail.com>
2022-01-11 14:33:22 +0800

    添加定时任务

--- lib/ui/view_model/yuemiao_view_model.dart
+++ lib/ui/view_model/yuemiao_view_model.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/widgets.dart';
+import 'dart:async';
+
 import 'package:hpv/model/entity/user_info.dart';
 import 'package:hpv/model/entity/yuemiao_hospital_info.dart';
 import 'package:hpv/net/service/yuemiao_service.dart';
@@ -7,6 +8,7 @@ import 'base/base_list_model.dart';
 
 class YMViewModel extends BaseListModel<YMHospitalInfo> {
   UserInfo? _userInfo;
+  Map<int, Timer> appointmentTaskMap = {};
 
   UserInfo? get userInfo => _userInfo;
 
@@ -25,5 +27,31 @@ class YMViewModel extends BaseListModel<YMHospitalInfo> {
     } finally {
       notifyListeners();
     }
+  }
+
+  /// 添加预约任务
+  void addAppointmentTask(YMHospitalInfo hospitalInfo) {
+    var id = hospitalInfo.id;
+    if (id == null) {
+      return;
+    }
+    if (appointmentTaskMap.containsKey(id)) {
+      var timer = appointmentTaskMap[id];
+      timer?.cancel();
+    }
+    appointmentTaskMap[id] = Timer.periodic(
+      const Duration(milliseconds: 500),
+      (timer) {
+
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    appointmentTaskMap.forEach((key, value) {
+      value.cancel();
+    });
+    super.dispose();
   }
 }
--- lib/ui/view_model/zhimiao_view_model.dart
+++ lib/ui/view_model/zhimiao_view_model.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:hpv/model/entity/product_info.dart';
 import 'package:hpv/model/entity/zhimiao_hospital_info.dart';
@@ -6,6 +8,8 @@ import 'package:hpv/net/service/zhimiao_service.dart';
 import 'base/base_list_model.dart';
 
 class ZMViewModel extends BaseListModel<ZMHospitalInfo> {
+  Map<int, Timer> appointmentTaskMap = {};
+
   @override
   Future<List<ZMHospitalInfo>> loadData() async {
     List<ZMHospitalInfo> list = [];
@@ -42,5 +46,29 @@ class ZMViewModel extends BaseListModel<ZMHospitalInfo> {
       debugPrint(e.toString());
       return [];
     }
+  }
+
+  /// 添加预约任务
+  void addAppointmentTask(ZMHospitalInfo hospitalInfo) {
+    var id = hospitalInfo.id;
+    if (id == null) {
+      return;
+    }
+    if (appointmentTaskMap.containsKey(id)) {
+      var timer = appointmentTaskMap[id];
+      timer?.cancel();
+    }
+    appointmentTaskMap[id] = Timer.periodic(
+      const Duration(milliseconds: 500),
+      (timer) {},
+    );
+  }
+
+  @override
+  void dispose() {
+    appointmentTaskMap.forEach((key, value) {
+      value.cancel();
+    });
+    super.dispose();
   }
 }
